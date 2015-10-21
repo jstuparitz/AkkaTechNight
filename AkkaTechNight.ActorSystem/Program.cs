bf73dbe88cde32f6fde15ab559e113ea6e071529
jstuparitz <jstuparitz@hotmail.com>
2015-10-20 23:43:31 -0500

    testing

--- AkkaTechNight.ActorSystem/Program.cs
+++ AkkaTechNight.ActorSystem/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace AkkaTechNight.ActorSystem
+﻿using System;
+using System.Collections.Generic;
+using Akka.Actor;
+using AkkaTechNight.Actors;
+
+namespace AkkaTechNight.ActorSystem
 {
     internal class Program
     {
+        private static Akka.Actor.ActorSystem MyActorSystem;
         private static void Main(string[] args)
         {
-            var mActorSystem = Akka.Actor.ActorSystem.Create("MyActorSystem");
+            MyActorSystem = Akka.Actor.ActorSystem.Create("MyActorSystem");
+            saList<IActorRef> drivers = CreateDrivers();
+
+            MyActorSystem.AwaitTermination();
         }
+
+        private static List<IActorRef> CreateDrivers()
+        {
+            List<IActorRef> list = new List<IActorRef>();
+            int x = 0;
+            while (x < 300000)
+            {
+                list.Add(MyActorSystem.ActorOf(Props.Create(() => new Driver(Guid.NewGuid()))));
+                x++;
+            }
+            return list;
+        } 
     }
 }
